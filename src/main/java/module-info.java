4b95c78f24200509ae56aef1c418ac9c21b76791
S1deprojects <182558216+S1deprojects@users.noreply.github.com>
2024-09-27 13:51:25 +0100

    removed unused dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,4 @@
 module com.example.mmsimgui {
     requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens com.example.mmsimgui to javafx.fxml;
     exports com.example.mmsimgui;
 }
